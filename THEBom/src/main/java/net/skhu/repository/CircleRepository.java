153231dac8b5e6a3ac314e6594c2d1b2dc6c6c92
holiday31 <holiday31@naver.com>
2020-01-02 20:13:25 +0900

    delete web all

--- THEBom/src/main/java/net/skhu/repository/CircleRepository.java
+++
@@ -1,9 +0,0 @@
-package net.skhu.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import net.skhu.document.Circle;
-
-public interface CircleRepository extends MongoRepository<Circle,String>  {
-
-}
--- THEBom/src/main/java/net/skhu/repository/SeniorCardInfoRepository.java
+++
@@ -1,9 +0,0 @@
-package net.skhu.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import net.skhu.model.nfc.SeniorCardInfo;
-
-public interface SeniorCardInfoRepository extends MongoRepository<SeniorCardInfo, String>{
-	SeniorCardInfo findByUid(String uid);
-}
